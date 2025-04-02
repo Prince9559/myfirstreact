2e3a621e9189512ea710d928be8dd56ebc4a5043
Prince Software <kumarprince95596186@gmail.com>
2025-04-02 17:45:59 +0530

    second react

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import './Home.css';
+function Home()
+{
+return(
+    <div className='pic'>
+        <h1 className='img'>Prince Kumar</h1>
+        <p >I am Prince</p>
+
+    </div>
+)
+};
+
+export default Home;
