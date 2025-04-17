e4289d9a81fd8d6c709a42fcd7b95c0507b01c21
Prince Software <kumarprince95596186@gmail.com>
2025-04-17 13:27:16 +0530

    product

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-// import Product from './Components/Product';
-// import './Components/Product';
+import Product from './Components/Product';
+import './Components/Product';
 
-import Sub from "./Components/Sub";
+// import Sub from "./Components/Sub";
 function App() {
   return (
     
     
     
-    //  <div className='container'>  
+     <div className='container'>  
      
-    //  <Product sname="Men's Facewash" pic="pic/men.jpg"></Product>
-    //  <Product sname="Facewash" pic="pic/nive.jpg"></Product>
-    //  <Product sname="Pimple Free" pic="pic/pim.jpg"></Product>
-    //  <Product sname="Slipper" pic="pic/chappal.jpg"></Product>
-    //  <Product sname="Shoe" pic="pic/shose.jpg"></Product>
+     <Product sname="Men's Facewash" pic="pic/men.jpg"></Product>
+     <Product sname="Facewash" pic="pic/nive.jpg"></Product>
+     <Product sname="Pimple Free" pic="pic/pim.jpg"></Product>
+     <Product sname="Slipper" pic="pic/chappal.jpg"></Product>
+     <Product sname="Shoe" pic="pic/shose.jpg"></Product>
 
 
-    //  <Product sname="Coconut" pic="pic/coco.jpg"></Product>
-    //  <Product sname="Mango" pic="pic/mang.jpg"></Product>
-    //  <Product sname="Book" pic="pic/book.jpg"></Product>
-    //  <Product sname="Banana" pic="pic/banana.jpg"></Product>
-    //  <Product sname="Orange" pic="pic/orange..jpg"></Product>
+     <Product sname="Coconut" pic="pic/coco.jpg"></Product>
+     <Product sname="Mango" pic="pic/mang.jpg"></Product>
+     <Product sname="Book" pic="pic/book.jpg"></Product>
+     <Product sname="Banana" pic="pic/banana.jpg"></Product>
+     <Product sname="Orange" pic="pic/orange..jpg"></Product>
 
-    //  </div> 
-<div>
+     </div> 
 
-  <Sub></Sub>
-
-</div>
 
   );
 }
